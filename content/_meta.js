61469301edf69347aa1581678c040d73ac02eaf7
Muhammed Yaseen <l2r.hydra@gmail.com>
2025-11-11 00:15:37 +0530

    DFG

--- content/_meta.js
+++ content/_meta.js
@@ -98,5 +98,14 @@ export default {
       copyPage: false,
       collapsed: true
     },
+  },
+  multicharacter: {
+    title: '👥 Prism Multi Character',
+    theme: {
+      breadcrumb: false,
+      pagination: true,
+      copyPage: false,
+      collapsed: true
+    },
   }
 }
